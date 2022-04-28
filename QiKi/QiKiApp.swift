d755e1343774ea2f4ff01cae16dbe2d837bb2ae0
EverShin <gkwk56@gmail.com>
2022-04-28 15:20:23 +0900

    QiKi Init

---
+++ QiKi/QiKiApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiKiApp.swift
+//  QiKi
+//
+//  Created by Shin yongjun on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct QiKiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
